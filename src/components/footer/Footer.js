f4c7e107cadbd0b6a457e8c878a109c6340f95e0
Berk <berkbeleli@gmail.com>
2022-12-25 00:07:16 +0300

    Added Footer component and its style

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,15 @@
+const Footer = () => {
+	return (
+	  <article className="footer">
+		<a
+		  href="https://github.com/berkbeleli/"
+		  target="_blank"
+		  rel="noreferrer"
+		>
+		  Designed & Built by Berk &copy;2022
+		</a>
+	  </article>
+	);
+  };
+  
+  export default Footer;
