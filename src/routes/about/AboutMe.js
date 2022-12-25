fdb44b8fa7f88c0bd77e07f76412c9bb3f45be9b
Berk <berkbeleli@gmail.com>
2022-12-25 20:35:29 +0300

    Created AboutMe component

---
+++ src/routes/about/AboutMe.js
@@ -0,0 +1,20 @@
+const AboutMe = () => {
+	return (
+	  <section>
+		<h2 className="title-font blue-text h2-tag">About Me</h2>
+		<p className="white-text p-tag">
+		  I am a full-stack web developer with 9 months of working experience. I
+		  combine my coding passion with technical skills and fully commit
+		  to consistency, meeting deadlines, and utilizing all available resources
+		  to achieve results. I enjoy creating beautiful, accessible, and
+		  responsive websites using HTML, CSS, Tailwind, Sass, SCSS, Vanilla
+		  JavaScript, React, Angular, Java, Spring. Effective use of time, problem-solving, and
+		  communication skills help me show a positive attitude in complex
+		  situations, and my desire to learn quickly adapts me to changing
+		  situations.
+		</p>
+	  </section>
+	);
+  };
+  export default AboutMe;
+  
