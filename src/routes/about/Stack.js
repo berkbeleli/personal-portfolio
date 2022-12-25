3ef098fd82a93b0be5fe8c573929c2cad70ac71e
Berk <berkbeleli@gmail.com>
2022-12-25 20:42:04 +0300

    Created Languages Stack

---
+++ src/routes/about/Stack.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Stack = () => {
+  return (
+    <React.Fragment>
+      <h3 className="title-font white-text ">
+        Here are a few tools & technologies I’ve been working with:
+      </h3>
+      <ol className="stack-section ">
+        <li>HTML5</li>
+        <li>CSS3</li>
+        <li>Tailwind</li>
+        <li>SCSS/SASS</li>
+        <li>JavaScript ES6+</li>
+        <li>React.js</li>
+		<li>Typescript</li>
+		<li>Angular</li>
+		<li>Java</li>
+		<li>Spring Framework</li>
+		<li>MySQL</li>
+		<li>PHP</li>
+        <li>RESTful API</li>
+        <li>Figma</li>
+        <li>Visual Studio Code</li>
+		<li>IntelliJ IDEA</li>
+      </ol>
+    </React.Fragment>
+  );
+};
+export default Stack;
