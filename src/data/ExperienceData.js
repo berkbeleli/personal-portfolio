bed9c6334c5973013bd168dfe081775ab6f4c313
Berk <berkbeleli@gmail.com>
2022-12-25 21:05:42 +0300

    Added a files to keep experience and Education page as json data

---
+++ src/data/ExperienceData.js
@@ -0,0 +1,110 @@
+const ExperienceData = [
+  {
+    id: 1,
+    jobtitle: "Software Engineer",
+    companyname: "Kabi Partners",
+    worktype: "Full-time",
+    duration: "Sep 2020 - May 2021",
+    location: "Izmir, Turkey",
+    responsibility: [
+      `Improved my professional Java knowledge.`,
+      `Improved my technical knowledge.`,
+      `Implemented my Web design knowledges`,
+    ],
+  },
+  {
+    id: 2,
+    jobtitle: "Software Developer Trainee",
+    companyname: "Mergentech",
+    worktype: "Full-time",
+    duration: "Jan 2020 - Feb 2020",
+    location: "Eskisehir, Turkey",
+    responsibility: [
+      `Improved my professional Java knowledge.`,
+      `Learned Responsive Website design principles.`,
+    ],
+  },
+  {
+    id: 3,
+    jobtitle: "Waitress",
+    companyname: "CanCan",
+    worktype: "Part-time",
+    duration: "Dec 2018 - Feb 2019",
+    location: "Vilnius, Lithuania",
+    responsibility: [
+      `Providing customer service. `,
+      `Greeting customers and taking food/drink orders. `,
+      `Making suggestions based on their preference.`,
+      `Keeping ashtrays clean and tables tidy. `,
+      `Cooperating between customers and the serving staff. `,
+    ],
+  },
+];
+
+const EducationData = [
+  {
+    name: "Vilnius Gediminas Technical University",
+    status: "Ba, Computer Engineer",
+    year: "2018 - 2019",
+  },
+  {
+    name: "Eskisehir Osmangazi University",
+    status: "Ba, Computer Engineer",
+    year: "2015 - 2020",
+  },
+];
+
+const CertificationData = [
+  {
+    name: "React + Redux",
+    source: "Sololearn",
+    date: "Issued 2022 · No Expiration Date",
+    creditUrl: "https://www.sololearn.com/certificates/CT-WP6JZBUP",
+  },
+  {
+    name: "Introduction to JavaScript",
+    source: "Sololearn",
+    date: "Issued 2022 · No Expiration Date",
+    creditUrl: "https://www.sololearn.com/certificates/CC-DGGXAN8P",
+  },
+  {
+    name: "Java",
+    source: "Sololearn",
+    date: "Issued 2022 · No Expiration Date",
+    creditUrl: "https://www.sololearn.com/certificates/CC-ZLIMVTUP",
+  },
+  {
+    name: "Jira Fundamentals Badge",
+    source: "Atlassian",
+    date: "Issued 2022 · No Expiration Date",
+    creditUrl:
+      "https://university.atlassian.com/student/award/kmM6LhqKdUyfZGVcvUftaNHS",
+  },
+  {
+    name: "Responsive Web Design",
+    source: "Sololearn",
+    date: "Issued 2022 · No Expiration Date",
+    creditUrl: "https://www.sololearn.com/certificates/CT-EKPPMTJI",
+  },
+  {
+    name: "Master the Coding Interview: Data Structures + Algorithms",
+    source: "Udemy",
+    date: "Issued 2022 · No Expiration Date",
+    creditUrl: "https://www.udemy.com/certificate/UC-db4c54b5-28c7-47fb-9e22-3f89c522f95a/",
+  },
+  {
+    name: "PHP",
+    source: "Sololearn",
+    date: "Issued 2022 · No Expiration Date",
+    creditUrl: "https://www.sololearn.com/certificates/CT-SLF0BYLY",
+  },
+  {
+    name: "Arcelik iOS Swift Bootcamp",
+    source: "Patika",
+    date: "Issued 2022 · No Expiration Date",
+    creditUrl: "https://verified.sertifier.com/en/verify/08974183863204?ref",
+  },
+
+];
+
+export { ExperienceData, EducationData, CertificationData };
