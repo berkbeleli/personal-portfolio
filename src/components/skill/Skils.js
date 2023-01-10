839e78f9887959ad9c86561dc9c25cac3ab7e4d5
Berk <berkbeleli@gmail.com>
2023-01-11 02:07:16 +0300

    Creates Skill.js and its stylesheet

---
+++ src/components/skill/Skils.js
@@ -0,0 +1,18 @@
+import { SkillsData } from "../../data/SkillsData";
+
+const Skills = () => {
+return (
+	<div className="toolbox">
+	{SkillsData.map((skill, index) => (
+		   <div className="toolbox__tool" key= {skill.id}>
+		   <div className="toolbox__tool-logo">
+				<skill.img className="svg" />
+		   </div>
+		   <div className="toolbox__tool-name">{skill.name}</div>
+		 </div>
+		))}
+	</div>
+)
+}
+
+export default Skills;
